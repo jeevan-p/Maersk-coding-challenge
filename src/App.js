022cc35bd9e88efb068a679547f75de559c493e3
Jeevan Paul Puthukka <jeevanpaulp@gmail.com>
2021-08-06 15:59:30 +0100

    Added functionality and added styles

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import Content from "./Components/Content/Content.jsx";
 import Numbers from "./Components/Numbers/Numbers.jsx";
 import Buttons from "./Components/Buttons/Buttons.jsx";
 import "./styles.scss";
-import content from "./labelData.json";
+import content from "./data.json";
+import shuffleFunction from "./Utils/shuffle";
+import { useState } from "react";
 
 export default function App() {
+  const [numberArrayState, setNumberArrayState] = useState(content.array);
   return (
     <div className="App">
       <Header text={content.headerText} />
       <Content>
-        <Numbers arrayOfNumbers={content.array} />
-        <Buttons />
+        <Numbers
+          arrayOfNumbers={numberArrayState}
+          tileColor={content.tileColor}
+        />
+        <Buttons
+          shuffleClicked={() =>
+            setNumberArrayState(shuffleFunction([...numberArrayState]))
+          }
+          sortClicked={() => setNumberArrayState(content.array)}
+          buttonTexts={content.buttonTexts}
+        />
       </Content>
     </div>
   );
